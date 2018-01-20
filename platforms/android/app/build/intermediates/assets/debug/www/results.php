c5137ae0dadd4b3bfa6a3673094ba0535df7198c
AlbertChen7 <alchen1818@gmail.com>
2018-01-19 16:15:59 -0800

    Front-end updates and unsucessful android attempts

---
+++ platforms/android/app/build/intermediates/assets/debug/www/results.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <h1>Your Answers</h1>
+  </head>
+  <body>
+    <?php
+       if(isset($_POST["submit"])) {
+         $team = $_POST["team"]
+       }
+
+    // function test_input($data) {
+    //   $data = trim($data);
+    //   $data = stripslashes($data);
+    //   $data = htmlspecialchars($data);
+    //   return $data;
+    // }
+    ?>
+  </body>
+</html>
