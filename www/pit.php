604bcef7dcd4acca837b5122d828de3d1df64a70
isaacaddis <isaacaddis21@gmail.com>
2018-03-10 22:56:43 -0800

    Backend for pitscouting

---
+++ www/pit.php
@@ -0,0 +1,30 @@
+<?php
+     header("Access-Control-Allow-Origin: *");
+     $con = mysqli_connect("104.197.101.9","root","nerdherd687","db");
+     if(!$con){
+       die('Error:'.mysqli_connect_error());
+     }
+     $pitgroup=$_POST["pitgroup"];
+     $pitteam=$_POST["pitteam"];
+     $pitauto=$_POST["pitauto"];
+     $pitautoabilities=$_POST["pitautoabilities"];
+     $pitteleop=$_POST["pitteleop"];
+     $pitintake=$_POST["pitintake"];
+     $pitscore=$_POST["pitscore"];
+     $pitclimb=$_POST["pitclimb"];
+     $pitimage=$_POST["pitimage"];
+
+      $result = mysqli_query($con, "DESCRIBE pitSCouting");
+      while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+        echo "{$row['Field']} - {$row['Type']}";
+      }
+      $q = mysqli_query($con, "INSERT INTO pitScouting () VALUES ()");
+
+      if($q) {
+        echo "Records inserted successfully.";
+      }
+      else {
+        echo "ERROR: Could not execute $q" .mysqli_error($con);
+      }
+
+?>
